b3352b4592a2ad7773039e452e70646982cd1c8a
Henter <a936946356@icloud.com>
2020-09-20 16:00:23 -0700

    StringSorterAsc is updated

--- src/StringSorterAsc.java
+++ src/StringSorterAsc.java
@@ -5,7 +5,28 @@ public class StringSorterAsc {
 
         public static void ContentSortAsc(ArrayList<Msg> list) {
 
-            
+            for (int i = 0; i < list.size(); i++) {
+                int idxMin = i;
+                for (int scan = i + 1; scan < list.size(); scan++) {
+                    if ((byte)list.get(idxMin).getContent().charAt(0)-(byte)(list.get(scan).getContent().charAt(0)) > 0) {
+                        idxMin = scan;
+                    }
+                    else if((byte)list.get(idxMin).getContent().charAt(0)-(byte)(list.get(scan).getContent().charAt(0)) == 0){
+                            if ((byte)list.get(idxMin).getContent().charAt(1)-(byte)(list.get(scan).getContent().charAt(1)) > 0) {
+                                    idxMin = scan;
+                            }
+                    }
+                    else if((byte)list.get(idxMin).getContent().charAt(1)-(byte)(list.get(scan).getContent().charAt(1)) == 0){
+                            if ((byte)list.get(idxMin).getContent().charAt(2)-(byte)(list.get(scan).getContent().charAt(2)) > 0) {
+                                    idxMin = scan;
+                            }
+                    }
+                    
+                    Msg a = list.get(i);
+                    list.set(i, list.get(idxMin));
+                    list.set(idxMin, a);
+                }
+            }
         }
 }
 
