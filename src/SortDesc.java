9a11bafb5ccebbce720630ea904fa91ae5e61862
Eloise Espel <eloise.espel@gmail.com>
2020-09-20 20:19:31 -0700

    Added basic method to sort integer by descending order

---
+++ src/SortDesc.java
@@ -0,0 +1,8 @@
+import java.util.Comparator;
+
+public class SortDesc implements Comparator<Integer>{
+	@Override
+    public int compare(Integer o1, Integer o2) {
+        return o2.compareTo(o1);
+    }
+}
