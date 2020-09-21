f9cbced00123ee749d5f94fed47b6af6d77718a8
Nicholas McGee <nicholas.r.mcgee@gmail.com>
2020-09-21 10:25:23 -0600

    Actually added StringSorterDesc

---
+++ src/StringSorterDesc.java
@@ -0,0 +1,25 @@
+public class StringSorterDesc {
+    /*
+     * Input: An array of strings
+     * Output: A new array with strings sorted in descending order
+     * Note: Case insensitive
+     * bubble sort is best sort
+     * */
+    public static String[] sortStringsDesc(String[] originalAr) {
+        String[] returnAr = originalAr.clone();
+
+        for (int i = returnAr.length - 1; i >= 0; i--) {
+            for (int j = i - 1; j >= 0; j--) {
+
+//              Swap function
+                if (returnAr[i].compareToIgnoreCase(returnAr[j]) > 0) {
+                    String temp = returnAr[i];
+                    returnAr[i] = returnAr[j];
+                    returnAr[j] = temp;
+                }
+            }
+        }
+
+        return returnAr;
+    }
+}
